be3ed8b2c3ba499a9090c658958a269cd5fad8fa
sachinspte <sachinpate9@gmail.com>
2020-01-13 22:42:39 +0530

    Added interface

---
+++ src/main/java/com/crm/qa/base/Constant.java
@@ -0,0 +1,11 @@
+package com.crm.qa.base;
+
+public interface Constant
+{
+     String CHROME_DRIVER_PATH="\\Drivers\\chromedriver.exe";
+     String FIREFOX_DRIVER_PATH="\\Drivers\\chromedriver.exe";
+     String PROP_PATH="\\src\\main\\resources\\";
+     long Page_Load_TimeOut = 20;
+     long Implicit_Wait = 30;
+     long SLEEP_WAIT=3000;
+}
