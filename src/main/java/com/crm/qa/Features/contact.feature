7d99a693ffc46ec58ffdd41c811ab06459e133ac
sachinspte <sachinpate9@gmail.com>
2020-01-14 23:54:20 +0530

    Added log folder

--- src/main/java/com/crm/qa/Features/contact.feature
+++ src/main/java/com/crm/qa/Features/contact.feature
@@ -5,4 +5,12 @@ Feature: Contact Page Details
     Then user click on New button and enters users details
     Then user click on save button
     Then verify Success message
+    Then close the brouser
 
+  Scenario: Deleting contact page details
+    Given user is on home page
+    When user click on conact page
+    When Select the user that is to be deleted
+    Then Click on Delete Item
+    Then Validate the delete message and contact page
+    Then close the brouser
--- test-output/report.js
+++ test-output/report.js
@@ -39,39 +39,121 @@ formatter.step({
   "name": "verify Success message",
   "keyword": "Then "
 });
+formatter.step({
+  "line": 8,
+  "name": "close the brouser",
+  "keyword": "Then "
+});
 formatter.match({
   "location": "ContactPageTest.user_is_on_home_page()"
 });
 formatter.result({
-  "duration": 54638501600,
+  "duration": 61297596600,
   "status": "passed"
 });
 formatter.match({
   "location": "ContactPageTest.user_click_on_conact_page()"
 });
 formatter.result({
-  "duration": 3312330300,
+  "duration": 4671548900,
   "status": "passed"
 });
 formatter.match({
   "location": "ContactPageTest.user_click_on_New_button_and_enters_users_details()"
 });
 formatter.result({
-  "duration": 5696701800,
+  "duration": 7211746300,
   "status": "passed"
 });
 formatter.match({
   "location": "ContactPageTest.user_click_on_save_button()"
 });
 formatter.result({
-  "duration": 295829500,
+  "duration": 197924800,
   "status": "passed"
 });
 formatter.match({
   "location": "ContactPageTest.verify_Success_message()"
 });
 formatter.result({
-  "duration": 44239400,
+  "duration": 103795600,
   "status": "passed"
+});
+formatter.match({
+  "location": "LoginPageTest.close_the_brouser()"
+});
+formatter.result({
+  "duration": 4648643500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Deleting contact page details",
+  "description": "",
+  "id": "contact-page-details;deleting-contact-page-details",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 11,
+  "name": "user is on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "user click on conact page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "Select the user that is to be deleted",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "Click on Delete Item",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 15,
+  "name": "Validate the delete message and contact page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 16,
+  "name": "close the brouser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactPageTest.user_is_on_home_page()"
+});
+formatter.result({
+  "duration": 34491938500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactPageTest.user_click_on_conact_page()"
+});
+formatter.result({
+  "duration": 7122466200,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({
+  "location": "LoginPageTest.close_the_brouser()"
+});
+formatter.result({
+  "status": "skipped"
 });
 });
